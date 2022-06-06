88962c4faa9c445c18c2afdfb0e11b20aefb1595
alex-bobrik <sasha.bobrik@inbox.ru>
2022-06-06 16:30:04 +0300

    Add WishList model

--- app/Http/Controllers/WishListController.php
+++ app/Http/Controllers/WishListController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\WishList;
+use App\Models\WishList;
 use Illuminate\Http\Request;
 
 class WishListController extends Controller
 {
     public function index()
     {
+        // create new WishList
+        $listToSave = new WishList;
+        $listToSave->wish_name = "test_name";
+        $listToSave->description = "des";
+        $listToSave->price = 0;
+        $listToSave->save();
+
         $wishLists = WishList::all();
 
         return view('lists', ['lists' => $wishLists]);
---
+++ app/Models/WishList.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WishList extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'wish_lists';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+      'wish_name',
+      'description',
+      'price',
+      'link'
+    ];
+
+}
