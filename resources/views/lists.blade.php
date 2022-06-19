2868a3f6c8cf99b4246d643b550c22369cb0c148
alex-bobrik <sasha.bobrik@inbox.ru>
2022-06-19 16:51:17 +0300

    Rename titles

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Wishes</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
